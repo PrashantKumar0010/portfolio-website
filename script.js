8982785c4c352a92bb20edd816306f868ebcee09
PRASHANTKUAMR <rdxprashant9876@gmail.com>
2024-07-16 19:58:08 +0530

    fix bug

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.Get
 document.querySelector('.mobile-navbar-btn').addEventListener('click', function (e) {
     document.querySelector('.header').classList.toggle('active');
     e.preventDefault();
